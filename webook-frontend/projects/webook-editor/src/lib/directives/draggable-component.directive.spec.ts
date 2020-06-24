1593e80999c054d8236b8e76b882fcbf2630e4ac
Eduardo Calixto <eduardo.calixto@viasoft.com.br>
2020-06-23 22:06:09 -0300

    Initial drag and drop

---
+++ webook-frontend/projects/webook-editor/src/lib/directives/draggable-component.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DraggableComponentDirective } from './draggable-component.directive';
+
+describe('DraggableComponentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DraggableComponentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
