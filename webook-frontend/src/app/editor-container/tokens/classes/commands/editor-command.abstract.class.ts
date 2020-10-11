dec9e898fd46a81608d3ed4b2891534440cebe47
unknown <eduardogubertcalixto@gmail.com>
2020-10-11 00:18:56 -0300

    Some small fixes

--- webook-frontend/src/app/editor-container/tokens/classes/commands/editor-command.abstract.class.ts
+++
@@ -1,7 +0,0 @@
-export abstract class EditorCommand {
-
-  public undo(): void { }
-
-  abstract execute(...args: any[]): void;
-}
-
--- webook-frontend/src/app/editor-container/tokens/classes/element/definition/editor-element-definition.class.ts
+++ webook-frontend/src/app/editor-container/tokens/classes/element/definition/editor-element-definition.class.ts
@@ -1,7 +1,8 @@
 import { Type } from '@angular/core';
+
 import {
   EditorBaseElement,
-} from 'src/app/editor-container/components/editor/editor-components/editor-element-base-classes/editor-base-element';
+} from '../../../../components/editor/editor-components/editor-element-base-classes/editor-base-element';
 
 export class EditorElementDefinition {
   elementClass: Type<EditorBaseElement>;
